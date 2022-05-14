f173792c95ffb02accf92bf665d1f2aa08664c03
Rogeliotoro <rogeltoro@gmail.com>
2022-05-14 18:24:25 +0200

    add table game

--- database/migrations/2022_05_14_161138_create-games_table.php
+++ database/migrations/2022_05_14_161138_create-games_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIsActiveToUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,7 @@ class AddIsActiveToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_active')->default(true);
-        });
+        //
     }
 
     /**
@@ -25,8 +23,6 @@ class AddIsActiveToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['is_active']);
-        });
+        //
     }
-}
+};
