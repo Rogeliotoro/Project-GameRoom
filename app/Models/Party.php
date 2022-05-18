4660c6b57072997cbef4ccc55fba2fbd894de50d
Rogeliotoro <rogeltoro@gmail.com>
2022-05-18 09:17:45 +0200

    created model party

---
+++ app/Models/Party.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Party extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'game_id',
+        'id_user',
+    ];
+
+    public function games()
+    {
+        return $this->belongsTo(Game::class, 'game_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+    public function messages()
+    {
+        return $this->hasMany(Message::class, 'party_id');
+    }
+};
